dea18b315ea4b12db25ab27b7a22a5d32ff70011
dangkhoa1209 <89619461+dangkhoa1209@users.noreply.github.com>
2021-10-20 13:34:00 +0700

    upload file

---
+++ config/configLoginGG.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "google_key": "492422101557-3oq2kq15kr8eifaur7tsp0k5uaovie9h.apps.googleusercontent.com", //Điền App ID của bạn vào đây
+    "google_secret": "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", //Điền App Secret ở đây
+    "callback_url": "http://localhost:8000/auth/google/callback"
+}
